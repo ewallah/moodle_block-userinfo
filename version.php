fc9a9d9862096e364b4b05709506ba407045494d
eWallah <info@eWallah.net>
2018-05-13 08:26:15 +0000

    version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018042600;
+$plugin->version = 2018042601;
 $plugin->requires = 2017050500;
 $plugin->release = '3.4+';
 $plugin->maturity = MATURITY_STABLE;
